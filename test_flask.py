edbf3c166a48f6c1bebaf402a8e9eca1089286f8
Jörn Preuß <joern.preuss@uni-siegen.de>
2019-12-18 17:09:13 +0100

    working page

--- test_flask.py
+++ test_flask.py
@@ -1,6 +1,9 @@
 # test_flask.py
 
 from flask import Flask, render_template, request
+from twhist.twhist import Twhist
+import csv
+
 # from app import app
 app = Flask('app')
 
@@ -8,10 +11,24 @@ app = Flask('app')
 @app.route('/', methods=['GET', 'POST'])
 def page():
 
-    a = request.form.get('a', '')
-    b = request.form.get('b', '')
+    query = request.form.get('query', '')
+    since = request.form.get('since', '')
+    until = request.form.get('until', '')
     # user = {'username': 'Miguel'}
-    return render_template('twhist.html', a=a, b=b)
+
+    twh = Twhist()
+
+    if query and since and until:
+        results = twh.get(
+            query, since, until,
+            limit_search=True, intervall='day')
+    else:
+        results = None
+
+    print(results)
+
+    return render_template(
+        'twhist.html', query=query, since=since, until=until, results=results)
 
 
 app.run(host='0.0.0.0', port=8080)
